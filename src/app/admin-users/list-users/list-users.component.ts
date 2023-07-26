d4a7148905ca187f70a020ee4a37c984013aaa19
p-grijalbar <p.grijalbar@uniandes.edu.co>
2023-07-25 21:49:27 -0500

    Listar usarios

--- src/app/admin-users/list-users/list-users.component.ts
+++ src/app/admin-users/list-users/list-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../../services/users/users.service';
 
 @Component({
   selector: 'app-list-users',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-users.component.css']
 })
 export class ListUsersComponent implements OnInit {
-
-  constructor() { }
+  users = [];
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.usersService.getUsers().subscribe((data) => {
+      this.users = data.users;
+    })
   }
 
 }
--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-
-  constructor() { }
+  // Define API
+  apiURL = environment.url;
+  constructor(private http: HttpClient) { }
+  getUsers() {
+    return this.http
+      .get<any>(this.apiURL + '/users')
+  }
 }
