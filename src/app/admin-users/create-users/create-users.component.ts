dcd0829b194df6ace2b834c6983ce7739c3d6648
nataliesantiagou <n.santiago@uniandes.edu.co>
2023-07-26 17:54:19 -0500

    integracion crear usuario

--- src/app/admin-users/create-users/create-users.component.ts
+++ src/app/admin-users/create-users/create-users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from "../../services/users/users.service";
+import {FormControl, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-users',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-users.component.css']
 })
 export class CreateUsersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  userForm: FormGroup;
+  constructor(private usersService: UsersService,
+              private router: Router) {
+    this.userForm = new FormGroup({
+      code: new FormControl(""),
+      name: new FormControl(""),
+      email: new FormControl(""),
+      role: new FormControl(""),
+      password: new FormControl(""),
+      status: new FormControl("")
+    });
   }
 
+  ngOnInit() {
+
+  }
+
+  createUser() {
+    const body = {
+      code: this.userForm.value.code,
+      email: this.userForm.value.email,
+      password: this.userForm.value.password,
+      role: this.userForm.value.role,
+      name: this.userForm.value.name,
+      status: this.userForm.value.status,
+    }
+
+    this.usersService.createUser(body).subscribe(data => {
+      if (data) {
+        this.router.navigateByUrl('/usuarios');
+      }
+    });
+  }
 }
